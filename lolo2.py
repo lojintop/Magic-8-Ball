411f5dcf5b6ea631fa8101fe7ba5e550f73e8552
login <lojinsalama05@gmail.com>
2025-03-28 14:42:34 +0200

    Create lolo2.py

---
+++ lolo2.py
@@ -0,0 +1,4 @@
+import random
+responses=["yes,definitely","NO,not now.","ASK agine later.","it is certain.","very doubtful","outlook is good","Beter not tell you now","concenttarate and ask agin."]
+def get_random_respones():
+ return random.choice(responses)
