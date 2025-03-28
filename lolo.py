81757dca2fb0f554d356d032bc986c95f169be26
login <lojinsalama05@gmail.com>
2025-03-28 14:55:54 +0200

    merge manual

--- lolo.py
+++ lolo.py
@@ -8,3 +8,8 @@ def get_user_guess():
                 print("please enter a number between 1and 100.")
         except ValueError:
             print("invalid input")
+            
+import random
+responses=["yes,definitely","NO,not now.","ASK agine later.","it is certain.","very doubtful","outlook is good","Beter not tell you now","concenttarate and ask agin."]
+def get_random_respones():
+ return random.choice(responses)
